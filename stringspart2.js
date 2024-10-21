bff50a40f370d482db1e8599057b78330af6e973
Your Name <you@example.com>
2024-10-21 23:04:40 +0530

    new files are added

---
+++ stringspart2.js
@@ -0,0 +1,24 @@
+//1.Relacing the substring “World!” with “My Dear Friend” ..
+let str="Hello World";
+let str2=str.replace("World", "My Dear Friend");
+console.log(str2);
+
+//2.Repeat the “Hungry” for 10 times
+let a="Hungry";
+let b="Hungry".repeat(10);
+console.log(b);
+
+//3.Finding the last index of the string “fox”
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+let lastindex=sentence.lastIndexOf("fox");
+console.log(lastindex);
+
+//4.Finding the string starts with “hi”
+let string="Hello, world!";
+let string2=string.startsWith("hi");
+console.log(string2);
+
+//5.Find the string ends with “.pdf”
+let filename= "document.pdf";
+let f2=filename.endsWith("pdf");
+console.log(f2);
